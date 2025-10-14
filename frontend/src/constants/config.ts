0edddfb7f438dae8667d75d6696453cf3e9eb39c
XuanGiaHanNguyen <han316@usf.edu>
2025-10-13 23:05:52 -0400

    setting up docker

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -2,5 +2,7 @@ export const GRID_SIZE = 50
 export const FPS = 10  // Animation speed (frame per second)
 export const BRIGHTNESS_THRESHOLD = 128
 export const RANDOM_PROBABILITY = 0.3
-export const API_ENDPOINT = "http://localhost:8080/pixelate" // backend api 
+export const API_ENDPOINT = import.meta.env.VITE_API_URL
+
+// "http://localhost:8080/pixelate" // backend api 
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+export const API_URL = process.env.VITE_API_URL || 'http://localhost:8080/pixelate'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
 })
+
