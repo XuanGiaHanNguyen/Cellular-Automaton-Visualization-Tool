58cf033d82e78ae658cfce66b77422651c490746
XuanGiaHanNguyen <han316@usf.edu>
2025-10-27 18:33:46 -0400

    added api endpoint

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -2,6 +2,6 @@ export const GRID_SIZE = 50
 export const FPS = 10  // Animation speed (frame per second)
 export const BRIGHTNESS_THRESHOLD = 128
 export const RANDOM_PROBABILITY = 0.3
-export const API_ENDPOINT = import.meta.env.VITE_API_URL || 'http://localhost:8080/pixelate'
+export const API_ENDPOINT = 'http://54.237.74.16:8080'
 
 
